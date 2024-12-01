cda82d6ef2adb35dbf2fa18b4ad1588fd6afd272
Zak Micallef <avatarziko619@gmail.com>
2024-12-01 11:57:02 +0100

    Bugfix. Backend is not dockerised!

--- NAVIE/Api.py
+++ NAVIE/Api.py
@@ -70,35 +70,50 @@ def get_adaptation_options_schema():
     return schema
 
 
+# @app.get("/execute_schema")
+# def get_execute_schema():
+#     schema = load_schema("execute_schema.json")
+#     return schema
+
+
+# @app.put("/execute")
+# def execute_action(option: str, new_value: float):
+#     try:
+#         df = pd.read_csv('knowledge.csv', header=None)
+#         option_map = {
+#             "yolov5n_rate_min": (0, 1), "yolov5n_rate_max": (0, 2),
+#             "yolov5s_rate_min": (1, 1), "yolov5s_rate_max": (1, 2),
+#             "yolov5m_rate_min": (2, 1), "yolov5m_rate_max": (2, 2),
+#             "yolov5l_rate_min": (3, 1), "yolov5l_rate_max": (3, 2),
+#             "yolov5x_rate_min": (4, 1), "yolov5x_rate_max": (4, 2)
+#         }
+#
+#         if option not in option_map:
+#             raise HTTPException(status_code=400, detail="Invalid adaptation option")
+#
+#         row, col = option_map[option]
+#         df.iat[row, col] = new_value
+#         df.to_csv('knowledge.csv', header=None, index=False)
+#
+#         return {"message": f"Adaptation option '{option}' updated to {new_value}."}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Failed to execute adaptation action: {e}")
+
 @app.get("/execute_schema")
 def get_execute_schema():
-    schema = load_schema("execute_schema.json")
+    schema = load_schema("new_execute_schema.json")
     return schema
 
-
 @app.put("/execute")
-def execute_action(option: str, new_value: float):
+def new_execute_action(option: str):
     try:
-        df = pd.read_csv('knowledge.csv', header=None)
-        option_map = {
-            "yolov5n_rate_min": (0, 1), "yolov5n_rate_max": (0, 2),
-            "yolov5s_rate_min": (1, 1), "yolov5s_rate_max": (1, 2),
-            "yolov5m_rate_min": (2, 1), "yolov5m_rate_max": (2, 2),
-            "yolov5l_rate_min": (3, 1), "yolov5l_rate_max": (3, 2),
-            "yolov5x_rate_min": (4, 1), "yolov5x_rate_max": (4, 2)
-        }
-        
-        if option not in option_map:
-            raise HTTPException(status_code=400, detail="Invalid adaptation option")
-        
-        row, col = option_map[option]
-        df.iat[row, col] = new_value
-        df.to_csv('knowledge.csv', header=None, index=False)
-        
-        return {"message": f"Adaptation option '{option}' updated to {new_value}."}
+        #creates Executor object and call's to perform action.
+        exe_obj = Executor()
+        exe_obj.perform_action(option)
+
+        return {"message": f"Adaptation update to '{option}' executed successfully'."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to execute adaptation action: {e}")
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
